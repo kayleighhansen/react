a331f0add1f3150d458356716483681c539eab49
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2022-03-10 16:12:06 -0700

    ...

--- modal/src/App.js
+++ modal/src/App.js
@@ -9,10 +9,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
 
+  // set articleData 
   const [articleData, setArticleData] = useState(null);
+  const [filteredArticleData, setFilteredArticleData] = useState(null);
+  const [displayArticleData, setDisplayArticleData] = useState(null);
+  
 
+  let state = { inputValue: '' };
+
+  let change = handleChange.bind(this);
+  let submit = handleSubmit.bind(this);
+
+  // fetch data, set to articleData
   useEffect(() => {
     setArticleData(null);
+    setDisplayArticleData(null);
     fetch('https://hoopcamp-dev.azurewebsites.net/events/getEvents', {
       method: 'GET',
       headers: {
@@ -21,16 +32,29 @@ function App() {
     }).then(data => {
       return data.json()
         .then(result => {
-          setArticleData(result.value);
-        })
+
+          let date = new Date('01 Jan 2022 00:00:00 GMT').getTime();
+          let articles = [];
+
+          result.value.forEach((item) => {
+
+            const database_date = new Date(item.new_starttime);
+
+            if(database_date > date) {
+              articles.push(item);
+            };
+          }); 
+          setArticleData(articles);
+          setDisplayArticleData(articles);
+        });
     }).catch(err => { console.log(err) });
-  }, []);
+  }, [])
 
   function renderEventList() {
-    if (articleData) {
+    if (displayArticleData) {
       return (
         <div>
-          <ArticleList events={articleData} />
+          <ArticleList events={displayArticleData} />
         </div>
       );
     } else {
@@ -40,6 +64,32 @@ function App() {
     }
   }
 
+  function handleChange(event) {
+    console.log(event.target.value);
+    let filteredArray = [];
+    articleData.forEach((article)=> {
+
+      if(article.msnfp_name.toLowerCase().includes(event.target.value)) {
+        filteredArray.push(article);
+      };
+    
+    });
+    console.log(filteredArray);
+    setFilteredArticleData(filteredArray);
+    setDisplayArticleData(filteredArray)
+
+    if(event.target.value == null) {
+      setFilteredArticleData(null);
+
+      setDisplayArticleData(articleData);
+    }
+  }
+
+  function handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   return (
     <div>
 
@@ -47,15 +97,14 @@ function App() {
 
       <div className="searchbar-container">
         <form action="" className="searchbar-form">
-          <input type="text" name="search" className="searchbar-input"></input>
-          <button type="submit" className="searchbar-button"><FontAwesomeIcon icon={faMagnifyingGlass} className="searchbar-icon"/></button>
+          <input type="text" name="search" className="searchbar-input" value={ state.value } onChange={handleChange}></input>
+          <button type="button" className="searchbar-button" ><FontAwesomeIcon icon={faMagnifyingGlass} className="searchbar-icon"/></button>
         </form>
       </div>
 
       { articleData !== null ? renderEventList() : renderEventList() }
 
     </div>);
-
 }
 
 export default App;
